905df0c0177dfceeac96a06f5e0966d224f808ce
Abdul Aziz <aziz12d97@gmail.com>
2021-02-02 23:25:42 +0600

    Truthy or Falsy

---
+++ TruthyVSFalsy.js
@@ -0,0 +1,19 @@
+//Falsy
+//False
+//0
+//undefined
+//""
+//null
+//NaN
+
+//Truthy
+//" ", [], {}
+
+let name = false;
+
+if(name){
+    console.log("this is true");
+}
+else{
+    console.log("This is False");
+}
